4c509b183e39f8e5d74337d2bb5f22bbb32a4365
sukanta das <154526973+swapnauran@users.noreply.github.com>
2024-03-18 09:52:32 +0530

    Update app.py

--- app.py
+++ app.py
@@ -123,4 +123,4 @@ def upscale_image():
             return jsonify({'error': error_message})
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host="0.0.0.0", port=5000, debug=True)
